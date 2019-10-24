2de18b21620d3274db11bce4ad6abe0e79c7a354
oguzhantomak <oguzhantomak@gmail.com>
2019-10-24 22:36:10 +0300

    Tasks done about Form3

---
+++ KararYapilari/Form3.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace KararYapilari
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRastgeleGelenDeger = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblKalanHak = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lütfen aşağıda görmüş karakterleri ilgili yazı alanına geçiriniz:";
+            // 
+            // lblRastgeleGelenDeger
+            // 
+            this.lblRastgeleGelenDeger.AutoSize = true;
+            this.lblRastgeleGelenDeger.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRastgeleGelenDeger.Location = new System.Drawing.Point(13, 39);
+            this.lblRastgeleGelenDeger.Name = "lblRastgeleGelenDeger";
+            this.lblRastgeleGelenDeger.Size = new System.Drawing.Size(28, 18);
+            this.lblRastgeleGelenDeger.TabIndex = 1;
+            this.lblRastgeleGelenDeger.Text = ". . .";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(16, 72);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(172, 20);
+            this.txt1.TabIndex = 0;
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(194, 72);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(109, 20);
+            this.btnGirisYap.TabIndex = 3;
+            this.btnGirisYap.Text = "GİRİŞ YAP";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kalan Giriş Hakkınız:";
+            // 
+            // lblKalanHak
+            // 
+            this.lblKalanHak.AutoSize = true;
+            this.lblKalanHak.Location = new System.Drawing.Point(231, 117);
+            this.lblKalanHak.Name = "lblKalanHak";
+            this.lblKalanHak.Size = new System.Drawing.Size(13, 13);
+            this.lblKalanHak.TabIndex = 5;
+            this.lblKalanHak.Text = "5";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 168);
+            this.Controls.Add(this.lblKalanHak);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblRastgeleGelenDeger);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRastgeleGelenDeger;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblKalanHak;
+    }
+}
---
+++ KararYapilari/Form3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KararYapilari
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int rastgele = rnd.Next(10000000, 100000000);
+            lblRastgeleGelenDeger.Text = rastgele.ToString();
+        }
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            int kalan = Convert.ToInt32(lblKalanHak.Text);
+            if (txt1.Text == lblRastgeleGelenDeger.Text)
+            {
+                MessageBox.Show("Tebrikler");
+            }
+            else
+            {
+                txt1.Clear();
+                txt1.Focus();
+                MessageBox.Show("Hata");
+                kalan--;
+                lblKalanHak.Text = kalan.ToString();
+                if (kalan == 0)
+                {
+                    MessageBox.Show("Deneme hakkınız kalmadı. \nLütfen Daha Sonra Tekrar Deneyiniz. !");
+                    btnGirisYap.Enabled = false;
+                }
+            }
+        }
+    }
+}
